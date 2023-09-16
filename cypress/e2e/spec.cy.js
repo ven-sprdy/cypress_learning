e95f51356cc98ba47d7e984b66fee339ef070c51
prasad417 <417.prasad@gmail.com>
2023-09-16 13:11:10 -0500

    Adding login test and notes.txt with basic cypress knowledge

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,30 @@
 describe('ParaBank Homepage', () => {
 
-  before(() => {
-    cy.visit('https://parabank.parasoft.com/parabank/index.htm');
+  beforeEach(() => {
+    cy.visit('https://opensource-demo.orangehrmlive.com')
   });
 
   it('Verify ParaBank title', () => {
-    cy.title().should('eq', 'ParaBank | Welcome | Online Banking')
+    cy.title().should('eq', 'OrangeHRM')
+  })
+
+
+  it('Verify ParaBank login', () => {
+    cy.get('input[name="username"]').type('admin')
+    cy.get('input[name="password"]').type('admin123')
+    cy.get('button[type="submit"]').click()
+    cy.title().should('eq', 'OrangeHRM')
+    cy.url().should('include', 'dashboard')
+      .and('contains', '/web/index.php/dashboard/index')
+
+    let expectUserName = 'Paul Collings'
+    cy.get('.oxd-userdropdown-name').then( (element) => {
+      let actualUserName = element.text()
+      expect(actualUserName).to.equal(expectUserName)
+
+      assert.equal(actualUserName, expectUserName)
+    })
+
   })
 
 })
