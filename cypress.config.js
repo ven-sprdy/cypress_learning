15c6c9d8ba596df3defb7c40d4176b6bf8afa7bc
prasad417 <417.prasad@gmail.com>
2023-09-17 08:52:12 -0500

    Report working commit, with before browser launch event in cypress congig

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,32 @@ const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 const now = new Date().toLocaleString();
+const fs = require("fs");
+
+function createReportJsonMeta(results) {
+  // let metadata_dir = "./cypress/reports/metadata";
+  // if (!fs.existsSync(metadata_dir)){
+  //   fs.mkdirSync(metadata_dir, { recursive: true });
+  // }
+
+  fs.writeFileSync(
+    "./cypress/reports/metadata/results.json",
+    JSON.stringify(
+      {
+        browserName: results.browserName,
+        browserVersion: results.browserVersion,
+        osName: results.osName,
+        osVersion: results.osVersion,
+        nodeVersion: results.config.resolvedNodeVersion,
+        cypressVersion: results.cypressVersion,
+        startedTestsAt: results.startedTestsAt,
+        endedTestsAt: results.endedTestsAt,
+      },
+      null,
+      "\t"
+    )
+  );
+}
 
 module.exports = defineConfig({
   e2e: {
@@ -13,7 +39,45 @@ module.exports = defineConfig({
 
       on("file:preprocessor", bundler);
       await addCucumberPreprocessorPlugin(on, config);
-      
+      on("before:browser:launch", () => {
+        let metadata_dir = ["./cypress/reports/json", "./cypress/reports/metadata"];
+        metadata_dir.forEach( (dir) => {
+          if (!fs.existsSync(dir)){
+            fs.mkdirSync(dir, { recursive: true });
+          }
+        });
+      });
+      // on("after:run", async (results) => {
+      //   if (results) {
+      //     createReportJsonMeta(results);
+      //     let sourcePath = "./cypress/reports/json";
+      //     let oldExtension = "cucumber.json";
+      //     let newExtension =
+      //       results.browserName + "." + new Date().getTime() + ".json";
+      //     fs.readdir(sourcePath, (err, files) => {
+      //       if (err) {
+      //         console.log("Issue in the file reading");
+      //         return;
+      //       }
+            
+      //       files.forEach((file) => {
+      //         const oldFilePath = `${sourcePath}/${file}`;
+      //         console.log(oldFilePath);
+      //         if (file.endsWith(`.${oldExtension}`)) {
+      //           const newFilePath = `${sourcePath}/${file.replace(
+      //             `.${oldExtension}`,
+      //             `.${newExtension}`
+      //           )}`;
+      //           fs.rename(oldFilePath, newFilePath, (err) => {
+      //             if (err) {
+      //               console.log("Issue in the file renaming");
+      //             }
+      //           });
+      //         }
+      //       });
+      //     });
+      //   }
+      // });
       // on('task', {
       //   beforeTest(testName) {
       //     console.log(`=== Starting Test: ${testName}, at ${now}`);
