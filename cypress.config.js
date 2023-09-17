07cc9a823cf33aaa7a46119cdc62682d0e20a616
prasad417 <417.prasad@gmail.com>
2023-09-16 20:06:08 -0500

    Cucumber_mocha_awsome_report

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,19 @@ const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir:'cypress/reports',
+    charts: true,
+    reportPageTitle: 'CypressReportBDD',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    debug: true
+  },
   e2e: {
     async setupNodeEvents(on, config) {
       const bundler = createBundler({
@@ -11,6 +23,9 @@ module.exports = defineConfig({
 
       on("file:preprocessor", bundler);
       await addCucumberPreprocessorPlugin(on, config);
+
+      on("file:preprocessor", browserify.default(config));
+      require('cypress-mochawesome-reporter/plugin')(on);
 
       return config;
     },
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,9 +14,10 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
+import 'cypress-mochawesome-reporter/register';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-require('cypress-xpath')
+require('cypress-xpath');
